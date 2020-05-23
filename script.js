5357f047f82bc785f2bd8f05aa5b0f1762acf006
carlmentz <63966055+carlmentz@users.noreply.github.com>
2020-05-23 14:08:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// question 1
+var firstVar;
 
+// question 2
+var name = "carl";
+
+// question 3
+var integer = 1;
+
+// question 4
+var division = 20 / 5;
+
+// question 5
+var frog = "amphibian";
+frog
+
+// question 6
+
+// question 7
+
+// question 8
